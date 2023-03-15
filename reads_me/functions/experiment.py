44e827c6f0a2bd21fb2e094613f2b4ed637bf3c9
Brian Risk <brian@geneffects.com>
2023-03-15 07:30:25 -0400

    adding IP notice, default image

--- reads_me/functions/experiment.py
+++ reads_me/functions/experiment.py
@@ -1,22 +1,5 @@
 import datetime
 
-import openai
-
-from reads_me.functions.wikipedia import is_person_dead, \
-    is_first_revision_in_past_month
-
-openai.api_key = "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"
-
-#
-# response = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=question,
-#         temperature=1,
-#         max_tokens=128,
-#     )
-#
-# print(response.choices[0].text)
-
 
 def days_since(date):
     """Returns the number of days since the input date up until the current date"""
